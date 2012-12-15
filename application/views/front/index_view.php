a05fbe3914826d0c2eb8a5fd55158629e9929648
Omar Terrazas <terrazas.omar@gmail.com>
2012-12-15 03:37:33 -0600

    Implementacion de estilos

--- application/views/front/index_view.php
+++ application/views/front/index_view.php
@@ -22,13 +22,13 @@
 					<li>Unlimited Monthly Bandwidth</li>
 					<li>CGI, PHP, RoR and MySQL</li>
 				</ul>
-				<h3>Start <span>$3.98</span> /mo</h3>
-				<a href="#">Get Started Now</a>
+				<div class="editable"><h3>Start <strong>$3.98</strong> /mo</h3></div>
+				<a href="#" class="button">Get Started Now</a>
 			</div>
 		</div>
 	</div>
 </div>
-<div id="search_domain">
+<!-- <div id="search_domain">
 	<div class="container">
 		<div>
 			<label>Find the Perfect Domain Name Today!</label>
@@ -44,95 +44,108 @@
 			<a href="#">See More Domain Pricing Click Here</a>
 		</div>
 	</div>
-</div>
+</div> -->
 <div id="plans">
 	<div class="container">
-		<h2>PLANES Y PRECIOS</h2>
-		<u>
-			<li><a href="#starter">STARTER</a></li>
-			<li><a href="#blogger">BLOGGER</a></li>
-			<li><a href="#bussines">BUSINESS</a></li>
-			<li><a href="#enterprise">ENTERPRISE</a></li>
-		</u>
-		<div id="plans_pricing">
-			<div class="pricing" id="starter">
-				<h3>Enterprise #1</h3>
-				<p>Starting <span>$99.99</span> /mo</p>
-				<ul>
-					<li>10GB Storage Space</li>
-					<li>50GB Bandwidth</li>
-					<li>Unlimited FTP & Email</li>
-					<li>cPanel Control Panel</li>
-				</ul>
-				<a href="">ORDENA AHORA</a>
-			</div>
-			<div class="pricing" id="starter">
-				<h3>Enterprise #2</h3>
-				<p>Starting <span>$62.90</span> /mo</p>
-				<ul>
-					<li>10GB Storage Space</li>
-					<li>50GB Bandwidth</li>
-					<li>Unlimited FTP & Email</li>
-					<li>cPanel Control Panel</li>
-				</ul>
-				<a href="">ORDENA AHORA</a>
-			</div>
-			<div class="pricing" id="starter">
-				<h3>Enterprise #3</h3>
-				<p>Starting <span>$75.90</span> /mo</p>
-				<ul>
-					<li>10GB Storage Space</li>
-					<li>50GB Bandwidth</li>
-					<li>Unlimited FTP & Email</li>
-					<li>cPanel Control Panel</li>
-				</ul>
-				<a href="">ORDENA AHORA</a>
-			</div>
-			<div class="pricing" id="starter">
-				<h3>Enterprise #4</h3>
-				<p>Starting <span>$83.90</span> /mo</p>
-				<ul>
-					<li>10GB Storage Space</li>
-					<li>50GB Bandwidth</li>
-					<li>Unlimited FTP & Email</li>
-					<li>cPanel Control Panel</li>
-				</ul>
-				<a href="">ORDENA AHORA</a>
-			</div>
+		<div class="editable"><h2>PLANES Y PRECIOS</h2></div>
+		<div id="planes" class="editable">
+			<ul>
+				<li class="active"><a href="#starter">STARTER</a></li>
+				<li><a href="#blogger">BLOGGER</a></li>
+				<li><a href="#bussines">BUSINESS</a></li>
+				<li><a href="#enterprise">ENTERPRISE</a></li>
+			</ul>
 		</div>
-		<h2>Why Prefer LiveWeb</h2>
-		<div id="prefer">
-			<div class="element_liveweb">
-				<img src="images/">
-				<div>
-					<h4>24 / 7 Professional Support</h4>
-					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+		<div class="clear"></div>
+		<div class="plans_pricing">
+			<div class="pricing" id="starter_1">
+				<div class="editable"><h3>Enterprise #1</h3></div>
+				<div class="editable"><p>Starting <strong>$99.99</strong> /mo</p></div>
+				<div class="editable">
+					<ul>
+						<li>10GB Storage Space</li>
+						<li>50GB Bandwidth</li>
+						<li>Unlimited FTP & Email</li>
+						<li>cPanel Control Panel</li>
+					</ul>
 				</div>
+				<a href="" class="button">ORDENA AHORA</a>
 			</div>
-			<div class="element_liveweb">
-				<img src="images/">
-				<div>
-					<h4>24 / 7 Professional Support</h4>
-					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+			<div class="pricing" id="starter_2">
+				<h3>Enterprise #2</h3>
+				<div class="editable"><p>Starting <strong>$62.90</strong> /mo</p></div>
+				<div class="editable">
+					<ul>
+						<li>10GB Storage Space</li>
+						<li>50GB Bandwidth</li>
+						<li>Unlimited FTP & Email</li>
+						<li>cPanel Control Panel</li>
+					</ul>
 				</div>
+				<a href="" class="button">ORDENA AHORA</a>
 			</div>
-			<div class="element_liveweb">
-				<img src="images/">
-				<div>
-					<h4>100% Network and Uptime</h4>
-					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+			<div class="pricing" id="starter_3">
+				<h3>Enterprise #3</h3>
+				<div class="editable"><p>Starting <strong>$75.90</strong> /mo</p></div>
+				<div class="editable">
+					<ul>
+						<li>10GB Storage Space</li>
+						<li>50GB Bandwidth</li>
+						<li>Unlimited FTP & Email</li>
+						<li>cPanel Control Panel</li>
+					</ul>
 				</div>
+				<a href="" class="button">ORDENA AHORA</a>
 			</div>
-			<div class="element_liveweb">
-				<img src="images/">
-				<div>
-					<h4>24 / 7 Professional Support</h4>
-					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+			<div class="pricing" id="starter_4">
+				<h3>Enterprise #4</h3>
+				<div class="editable"><p>Starting <strong>$83.90</strong> /mo</p></div>
+				<div class="editable">
+					<ul>
+						<li>10GB Storage Space</li>
+						<li>50GB Bandwidth</li>
+						<li>Unlimited FTP & Email</li>
+						<li>cPanel Control Panel</li>
+					</ul>
 				</div>
+				<a href="" class="button">ORDENA AHORA</a>
+			</div>
+			<div class="clear"></div>
+		</div>
+		<div class="editable"><h2>Why Prefer LiveWeb</h2></div>
+			<div id="prefer">
+				<div  id="service_red" class="element_liveweb editable">
+					<span></span>
+					<div class="editable">
+						<h4>24 / 7 Professional Support</h4>
+						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+					</div>
+				</div>
+				<div  id="service_yellow" class="element_liveweb editable">
+					<span></span>
+					<div class="editable">
+						<h4>24 / 7 Professional Support</h4>
+						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+					</div>
+				</div>
+				<div  id="service_green" class="element_liveweb editable">
+					<span></span>
+					<div class="editable">
+						<h4>100% Network and Uptime</h4>
+						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+					</div>
+				</div>
+				<div  id="service_gray" class="element_liveweb editable">
+					<span></span>
+					<div class="editable">
+						<h4>24 / 7 Professional Support</h4>
+						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+					</div>
+				</div>
+				<div class="clear"></div>
 			</div>
 		</div>
 	</div>
-</div>
 
 
 
